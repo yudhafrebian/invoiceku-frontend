c6dc8b363718d14cec5f2b57f6a9573c66ce07f8
Yudha Frebian <anandayudha777@gmail.com>
2025-06-05 16:52:50 +0700

    feat(invoices): implement data table for invoice management; add API integration for fetching invoices

---
+++ src/app/dashboard/invoices/columns.tsx
@@ -0,0 +1,169 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { apiCall } from "@/utils/apiHelper";
+import EditClientForm from "@/view/components/client/editClientForm";
+import { ColumnDef } from "@tanstack/react-table";
+import { Edit, Trash } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export type Invoice = {
+  id: string;
+  client: string;
+  start_date: Date;
+  due_date: Date;
+  total: number;
+  payment_method: string;
+  status: string;
+};
+
+export const columns = (
+  setRefresh: React.Dispatch<React.SetStateAction<boolean>>
+): ColumnDef<Invoice>[] => [
+  {
+    accessorKey: "client",
+    header: "Client",
+  },
+  {
+    accessorKey: "start_date",
+    header: "Start Date",
+  },
+  {
+    accessorKey: "due_date",
+    header: "Due Date",
+  },
+  {
+    accessorKey: "total",
+    header: "Total",
+  },
+  {
+    accessorKey: "payment_method",
+    header: "Payment Method",
+    cell: ({ row }) => {
+      const formatPaymentMethod = (method: string) => {
+        if (method === "QRIS") return "QRIS";
+        return method
+          .toLowerCase()
+          .split("_")
+          .map((word) => word[0].toUpperCase() + word.slice(1))
+          .join(" ");
+      };
+      return formatPaymentMethod(row.getValue("payment_method"));
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  // {
+  //   id: "actions",
+  //   header: "Actions",
+  //   cell: ({ row }) => {
+  //     const client = row.original;
+  //     const [open, setOpen] = useState(false);
+  //     const [openDialog, setOpenDialog] = useState(false);
+
+  //     const deleteClient = async () => {
+  //       const token = localStorage.getItem("token");
+  //       try {
+  //         const response = await apiCall.patch(
+  //           `/client/delete-client/${client.id}`,
+  //           null,
+  //           {
+  //             headers: {
+  //               Authorization: `Bearer ${token}`,
+  //             },
+  //           }
+  //         );
+
+  //         toast.success("Client Deleted", {
+  //           description: response.data.message,
+  //         });
+
+  //         setOpenDialog(false);
+  //         setRefresh((prev) => !prev);
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     };
+  //     return (
+  //       <div className="flex gap-2">
+  //         <Dialog>
+  //           <Tooltip>
+  //             <TooltipTrigger asChild>
+  //               <DialogTrigger asChild>
+  //                 <Button size={"icon"} variant={"outline"}>
+  //                   <Edit />
+  //                 </Button>
+  //               </DialogTrigger>
+  //             </TooltipTrigger>
+  //             <TooltipContent>
+  //               <p>Edit</p>
+  //             </TooltipContent>
+  //           </Tooltip>
+  //           <DialogContent>
+  //             <DialogHeader>
+  //               <DialogTitle>Edit Client</DialogTitle>
+  //               <DialogDescription>
+  //                 Update Your Client Information
+  //               </DialogDescription>
+  //             </DialogHeader>
+  //             <EditClientForm
+  //               onClose={() => setOpen(false)}
+  //               params={{ id: client.id }}
+  //               onSuccess={() => setRefresh((prev) => !prev)}
+  //             />
+  //           </DialogContent>
+  //         </Dialog>
+  //         <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+  //           <Tooltip>
+  //             <TooltipTrigger asChild>
+  //               <DialogTrigger asChild>
+  //                 <Button size={"icon"} variant={"destructive"}>
+  //                   <Trash />
+  //                 </Button>
+  //               </DialogTrigger>
+  //             </TooltipTrigger>
+  //             <TooltipContent>
+  //               <p>Delete</p>
+  //             </TooltipContent>
+  //           </Tooltip>
+  //           <DialogContent>
+  //             <DialogHeader>
+  //               <DialogTitle>Delete Client</DialogTitle>
+  //               <DialogDescription>
+  //                 Are you sure you want to delete?
+  //               </DialogDescription>
+  //               <div className="flex justify-end gap-2">
+  //                 <Button variant={"destructive"} onClick={deleteClient}>
+  //                   Delete
+  //                 </Button>
+  //                 <Button
+  //                   variant={"outline"}
+  //                   onClick={() => setOpenDialog(false)}
+  //                 >
+  //                   Cancel
+  //                 </Button>
+  //               </div>
+  //             </DialogHeader>
+  //           </DialogContent>
+  //         </Dialog>
+  //       </div>
+  //     );
+  //   },
+  // },
+];
--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,16 +1,48 @@
 "use client";
-import { withAuth } from '@/hoc/withAuth';
-import * as React from 'react';
+import { withAuth } from "@/hoc/withAuth";
+import * as React from "react";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+import { apiCall } from "@/utils/apiHelper";
 
-interface IInvoiceProps {
-}
+interface IInvoiceProps {}
 
 const Invoices: React.FunctionComponent<IInvoiceProps> = (props) => {
+  const [data, setData] = React.useState<any[]>([]);
+  const [refresh, setRefresh] = React.useState(false);
+
+  const getInvoice = async () => {
+    try {
+      const token = window.localStorage.getItem("token");
+      const response = await apiCall.get("/invoice/all-invoice", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log(response.data.data);
+      setData(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    getInvoice();
+  }, [refresh]);
   return (
-    <div>
-      <h1>Invoices Page</h1>
+    <div className="p-4 md:p-10 flex flex-col gap-4">
+      <div>
+        <h1 className="font-bold text-lg md:text-2xl text-primary">
+          Manage Invoices
+        </h1>
+        <p className="text-muted-foreground text-sm md:text-base">
+          Here you can manage your invoices
+        </p>
+      </div>
+      <DataTable columns={columns(setRefresh)} data={data} />
     </div>
-  )
+  );
 };
 
 export default withAuth(Invoices);
