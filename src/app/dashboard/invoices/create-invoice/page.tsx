3e87d8b600d24df417ac1b865c1802838abb130b
yudhafrebian <anandayudha777@gmail.com>
2025-07-01 21:48:04 +0700

    feat: add user verification check before accessing invoice and product pages

--- src/app/dashboard/invoices/create-invoice/page.tsx
+++ src/app/dashboard/invoices/create-invoice/page.tsx
@@ -1,14 +1,24 @@
 "use client";
+import { useAppSelector } from "@/app/hook";
 import { withAuth } from "@/hoc/withAuth";
 import CreateInvoiceForm from "@/view/components/invoice/CreateInvoiceForm";
-import SelectClient from "@/view/components/invoice/CreateInvoiceForm";
+import { useRouter } from "next/navigation";
 import * as React from "react";
 
 interface ICreateInvoicePageProps {}
 
-const CreateInvoicePage: React.FunctionComponent<ICreateInvoicePageProps> = (
-  props
-) => {
+const CreateInvoicePage: React.FunctionComponent<ICreateInvoicePageProps> = () => {
+  const user = useAppSelector((state) => state.authState);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (!user.is_verified) {
+      router.push("/dashboard/invoices");
+    }
+  }, [user.is_verified, router]);
+
+  if (!user.is_verified) return null;
+
   return (
     <div className="p-4 md:p-10 flex flex-col gap-4">
       <h1 className="font-bold text-lg md:text-2xl text-primary">
